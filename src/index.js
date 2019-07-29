b199771d7982a3c87a52f303d15cf7a8995ffc2a
qingyangmoke <qingyangmoke@qq.com>
2019-07-29 18:19:56 +0800

    修复数组整数索引的问题

--- src/index.js
+++ src/index.js
@@ -4,13 +4,7 @@
  * @return {boolean} true or false
  */
 module.exports = function deepQuery(target, ...args) {
-  return args.reduce((obj, props) => {
-    if (obj && props) {
-      return obj[props];
-    } else {
-      return obj;
-    }
-  }, target);
+  return args.reduce((obj, props) => (obj ? obj[props] : obj), target);
 };
 
 module.exports.default = module.exports;
